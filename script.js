ccea28231ae3d7f3e22d4ab6a19c21c64acc00fb
monike23 <monike.carmo.silva@escola.pr.gov.br>
2025-09-22 08:04:49 -0300

    lo

--- script.js
+++ script.js
@@ -1,5 +1,48 @@
-import { PALAVRAS_RUINS } from "./palavrasruins";
+import ( PALAVRAS_RUINS } from "./palavrasRuins.js";
+    
+const botaMostraPalavras = document.queryselector ('#botao-palavrachave');
 
-const botaoMostraPalavras = document.querySelector(#botao-palavrachave);
+botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
 
-botaoMostraPalavras.addEventListener(clik)
+function mostraPalavrasChave) (
+  const texto = document. querySelector ('#entrada-de-texto') .value;
+  const campoResultado = document. querySelector ('#resultado-palavrachave');
+  const palavraschave = processatexto(texto);
+
+  campoResultado.textContent = palavraschave.join(*, *);
+}
+function processatexto(texto) (
+  let palavras = texto.split(/AP(L)+/u);
+
+  for (let i in palavras) {
+    palavras [1] = palavras [1]. toLowerCase();
+  
+  }
+
+  palavras = tiraPalavrasRuins (palavras);
+
+  const frequencias = contaFrequencias (palavras);
+
+let ordenadas = Object.keys(frequencias). sort(ordenaPalavra);
+
+  function ordenaPalavra(p1, p2) { 
+     return frequencias[p2] - frequencias [p1];
+
+  }
+   return ordenadas slice(0, 10);
+}
+
+ function contaFrequencias(palavras) {
+   let frequencias = {};
+   for (let i of palavras) {
+     frequencias [i] = 0;
+   for (let j of palavras) {
+       if (i == j) {
+frequencias [i]++;
+       }
+    }
+}
+   return frequencias;
+}
+
+   
